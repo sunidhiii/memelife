4ff68f21b2dfd5a116dba78b23950c46b945c570
unknown <maheshwaran@ryuklabs.io>
2024-02-08 13:47:10 +0530

    RSN-351 Develop a listener that listens to the ETH contract for any transaction using the user connected wallet address and amount deposited in the EVM chain

---
+++ src/sol_event_log.js
@@ -0,0 +1,24 @@
+import { Connection, PublicKey } from "@solana/web3.js";
+const searchAddress = "ALkXoJ7daxtcE7sauzg8NVpwKQ8q8aE7AdoyMzX9FTTY";
+const endPoint ='https://crimson-thrumming-grass.solana-mainnet.quiknode.pro/ff7ae722ce0ac86c3fa1456a05bee9e97170f81d/'
+const solanaConnection = new Connection(endPoint);
+const getTransactions = async(address, numTx) => {
+    const pubKey = new PublicKey(address);
+    let transactionList = await solanaConnection.getSignaturesForAddress(pubKey, {limit:numTx});
+    transactionList.forEach((transaction, i) => {
+        const date = new Date(transaction.blockTime*1000);
+        console.log(`Transaction No: ${i+1}`);
+        console.log(`Signature: ${transaction.signature}`);
+        console.log(`Time: ${date}`);
+        console.log(`Status: ${transaction.confirmationStatus}`);
+        console.log(("-").repeat(20));
+    })
+}
+const getTransactions_logs = async() => {
+  let transaction = await solanaConnection.onAccountChange(new PublicKey("9qAFHpSVmi2sKCYcJt9mvAzzfjCCbF1YczSirsh3BooT"),
+    (updatedAccountInfo, context) => console.log("Updated account info: ", updatedAccountInfo),
+    "confirmed"
+  );
+}
+getTransactions_logs()
+getTransactions(searchAddress,5)
