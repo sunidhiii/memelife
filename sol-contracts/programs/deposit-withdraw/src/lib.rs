96c31b7e5ae45e3f198821b5cfe3c32c925e8d8c
unknown <maheshwaran@ryuklabs.io>
2024-02-19 18:48:02 +0530

    RSN-598 Optimization Fixes on UserPayment

--- sol-contracts/programs/deposit-withdraw/src/lib.rs
+++ sol-contracts/programs/deposit-withdraw/src/lib.rs
@@ -1,17 +1,18 @@
+
 use anchor_lang::prelude::*;
+use anchor_lang::Result;
 use std::ops::DerefMut;
 
 declare_id!("7Xev9w6cvHkDXmDFB71LBWXP99bNXqnccTKXz2QZEeRz");
 
 #[program]
-pub mod deposit_withdraw {
+pub mod memelife {
     use super::*;
-
 
     #[derive(Accounts)]
     #[instruction(nonce: u8)]
     pub struct Initialize<'info> {
-        authority: UncheckedAccount<'info>,
+        pub authority: UncheckedAccount<'info>,
         owner: Signer<'info>,
         #[account(
             seeds = [
@@ -33,7 +34,7 @@ pub mod deposit_withdraw {
         system_program: Program<'info, System>,
     }
 
-    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> ProgramResult {
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
         if amount == 0 {
             return Err(ErrorCode::InvalidAmount.into());
         }
@@ -43,7 +44,7 @@ pub mod deposit_withdraw {
         Ok(())
     }
 
-    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> ProgramResult {
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
         let seeds = &[
             ctx.accounts.pool.to_account_info().key.as_ref(),
             &[ctx.accounts.pool.nonce],
@@ -55,20 +56,16 @@ pub mod deposit_withdraw {
             return Err(ErrorCode::NotEnoughPoolAmount.into());
         }
 
-        // Get the total deposited amount from the pool
         let _total_amount = ctx.accounts.pool.deposited_amount;
 
-        // Define the percentages for each wallet
         let percentage_wallet1 = ctx.accounts.pool.percentage_wallet1;
         let percentage_wallet2 = ctx.accounts.pool.percentage_wallet2;
         let percentage_wallet3 = ctx.accounts.pool.percentage_wallet3;
 
-        // Calculate the amount to be transferred to each wallet based on the percentages
         let transfer_amount_wallet1 = (amount * percentage_wallet1 as u64) / 100;
         let transfer_amount_wallet2 = (amount * percentage_wallet2 as u64) / 100;
         let transfer_amount_wallet3 = (amount * percentage_wallet3 as u64) / 100;
 
-        // Create system transfer instructions for each transfer
         let ix_to_wallet1 = anchor_lang::solana_program::system_instruction::transfer(
             &ctx.accounts.vault.key(),
             &ctx.accounts.wallet1.key(),
@@ -87,40 +84,45 @@ pub mod deposit_withdraw {
             transfer_amount_wallet3,
         );
 
-        // Invoke the transfer instructions
-        anchor_lang::solana_program::program::invoke(&ix_to_wallet1, &[
-            ctx.accounts.vault.to_account_info(),
-            ctx.accounts.wallet1.to_account_info(),
-        ])?;
+        anchor_lang::solana_program::program::invoke(
+            &ix_to_wallet1,
+            &[
+                ctx.accounts.vault.to_account_info(),
+                ctx.accounts.wallet1.to_account_info(),
+            ],
+        )?;
 
-        anchor_lang::solana_program::program::invoke(&ix_to_wallet2, &[
-            ctx.accounts.vault.to_account_info(),
-            ctx.accounts.wallet2.to_account_info(),
-        ])?;
+        anchor_lang::solana_program::program::invoke(
+            &ix_to_wallet2,
+            &[
+                ctx.accounts.vault.to_account_info(),
+                ctx.accounts.wallet2.to_account_info(),
+            ],
+        )?;
 
-        anchor_lang::solana_program::program::invoke(&ix_to_wallet3, &[
-            ctx.accounts.vault.to_account_info(),
-            ctx.accounts.wallet3.to_account_info(),
-        ])?;
+        anchor_lang::solana_program::program::invoke(
+            &ix_to_wallet3,
+            &[
+                ctx.accounts.vault.to_account_info(),
+                ctx.accounts.wallet3.to_account_info(),
+            ],
+        )?;
 
         Ok(())
     }
 
-    // Add a method to modify the percentages if needed
     pub fn modify_percentages(
         ctx: Context<ModifyPercentages>,
         percentage_wallet1: u8,
         percentage_wallet2: u8,
         percentage_wallet3: u8,
-    ) -> ProgramResult {
+    ) -> Result<()> {
         let pool = ctx.accounts.pool.deref_mut();
 
-        // Check if the caller is the owner
         if *ctx.accounts.owner.key != pool.authority {
             return Err(ErrorCode::Unauthorized.into());
         }
 
-        // Check if the percentages are within a valid range (0-100) and if they add up to 100
         if percentage_wallet1 > 100
             || percentage_wallet2 > 100
             || percentage_wallet3 > 100
@@ -136,32 +138,6 @@ pub mod deposit_withdraw {
 
         Ok(())
     }
-}
-
-#[derive(Accounts)]
-#[instruction(nonce: u8)]
-pub struct Initialize<'info> {
-    authority: UncheckedAccount<'info>,
-    owner: Signer<'info>,
-    #[account(
-        seeds = [
-            pool.to_account_info().key.as_ref(),
-        ],
-        bump = nonce,
-    )]
-    pool_signer: UncheckedAccount<'info>,
-
-    #[account(mut)]
-    pool: Box<Account<'info, Pool>>,
-    #[account(
-        mut,
-        seeds = [
-            pool.to_account_info().key.as_ref(),
-        ],
-        bump = nonce,
-    )]
-    vault: AccountInfo<'info>,
-    system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
@@ -223,7 +199,7 @@ pub struct Pool {
     pub percentage_wallet1: u8,
     pub percentage_wallet2: u8,
     pub percentage_wallet3: u8,
-    pub deposited_amount: u64, // Track the total deposited amount
+    pub deposited_amount: u64,
 }
 
 #[derive(Accounts)]
@@ -233,7 +209,7 @@ pub struct ModifyPercentages<'info> {
     pool: Box<Account<'info, Pool>>,
 }
 
-#[error]
+#[error_code]
 pub enum ErrorCode {
     #[msg("Pool amount is not enough.")]
     NotEnoughPoolAmount,
@@ -247,3 +223,29 @@ pub enum ErrorCode {
     #[msg("Invalid percentages, they should add up to 100")]
     InvalidPercentages,
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
